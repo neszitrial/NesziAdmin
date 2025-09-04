afee2c308c6980d0d646a9ddfc539c4dc210d813
Nida <tmarn2004@gmail.com>
2025-09-04 05:53:30 +0300

    trial3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+  root: "./",
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        addItem: path.resolve(__dirname, "pages/add-items.html"),
+        listItem: path.resolve(__dirname, "pages/list-items.html"),
+        orders: path.resolve(__dirname, "pages/orders.html"),
+      },
+    },
+  },
+
   resolve: {
     alias: {
       "html5-qrcode": "html5-qrcode",
